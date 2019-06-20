bce91b558cc297dac5d366d43a73f9fd0a71dd2c
frodriguez <frodriguez@solati.com.co>
2019-06-20 18:26:15 -0400

    se crea constructor para la clase

--- experiencia.php
+++ experiencia.php
@@ -1,51 +1,83 @@
 <?php
 
   /**
-   *
+   *@author felipe rodriguez
    */
+
   class experienciaLaboral
   {
 
       private $nombreProfesion;
       public $descripcion;
-      public $visible;
+      public $visible = true;
       public $meses;
+
+
+      /*creamos el constructor de la clase*/
+
+      public function __construct($nombreProfesion, $descripcion)
+      {
+          $this->conjuntoProfesiones($nombreProfesion);
+          $this->descripcion = $descripcion;
+      }
 
       public function conjuntoProfesiones($nombreProfesion)
       {
-        $this->nombreProfesion = $nombreProfesion;
+        /*validacion para que no acepte cadenas vacias*/
+        if ($nombreProfesion == '') {
+          $this->nombreProfesion = 'No se pudo encontrar el registro';
+        }else{
+          $this->nombreProfesion = $nombreProfesion;
+        }
+
       }
 
       public function obtenerProfesion()
       {
         return $this->nombreProfesion;
       }
+
+      public function obtenerTiempoLaborado()
+      {
+        $year = floor($this->meses / 12);
+        //para aproximar el numero y no dejarlo con tantosdecimales utilizamos floor
+        $meseExtra = $this->meses % 12;
+
+        if($year == 0){
+
+          return ("$meses meses");
+
+        }elseif($meseExtra == 0){
+
+          return ("$year años");
+
+        }else{
+
+          return ("$year años y $meseExtra meses");
+        }
+
+      }
   }
 
-  $job1 = new experienciaLaboral();
-  $job1->conjuntoProfesiones('PHP Developer');
-  $job1->descripcion = 'Desarrollador en Solati Empresa que ofrece un software de gestión de cobranza Adminfo.';
-  $job1->visible = true;
+  //le enviamos los datos al constructor como parametros
+
+  $job1 = new experienciaLaboral('PHP Developer', 'Desarrollador en Solati Empresa que ofrece un software de gestión de cobranza Adminfo.');
   $job1->meses = 48;
 
 
-  $job2 = new experienciaLaboral();
-  $job2->conjuntoProfesiones('JavaScript Developer');
-  $job2->descripcion = 'Manejo del lenguajes de programacion JavaScript junto con herramientas del mismo como jquery, angulas, nodejs, reac native';
-  $job2->visible = true;
+  $job2 = new experienciaLaboral('JavaScript Developer','Manejo del lenguajes de programacion JavaScript junto con herramientas del mismo como jquery, angulas, nodejs, reac native');
   $job2->meses = 24;
+
+  $job3 = new experienciaLaboral('','Creador de interfaz graficas responsives para los diferentes tamaños de sus dispositivos');//Front End Developer
+  $job3->meses = 12;
 
 
   $jobs = [
       $job1,
-      $job2,     // $experiencia2
+      $job2,
+      $job3
 /*
-  	[
-  		'nombreProfesion'=>'Front End Developer',
-  		'descripcion' => 'Creador de interfaz graficas responsives para los diferentes tamaños de sus dispositivos',
-  		'visible' => true,
-  		'meses' => 25
-      ]/*,
+
       [
   		'nombreProfesion'=>'Soporte computación',
   		'descripcion' => 'aplicacion de mantenimiento preventivo y correctivo de los equipos de computo',
@@ -54,30 +86,10 @@
       ]*/
   ];
 
-  function obtenerTiempoLaborado($meses)
-  {
-    $year = floor($meses / 12);
-    //para aproximar el numero y no dejarlo con tantosdecimales utilizamos floor
-    $meseExtra = $meses % 12;
 
-    if($year == 0){
-
-      return ("$meses meses");
-
-    }elseif($meseExtra == 0){
-
-      return ("$year años");
-
-    }else{
-
-      return ("$year años y $meseExtra meses");
-    }
-
-  }
 
   function imprimirExperiencia($job)
   {
-
 
       if ($job->visible == false) {
         return;//si se cumple la condicion salte de la funcion
@@ -86,7 +98,7 @@
       echo '<li class="work-position">';
         echo '<h5>'. $job->obtenerProfesion(). '</h5>';
         echo '<p>'. $job->descripcion.'</p>';
-        echo '<p>'. obtenerTiempoLaborado($job->meses) . '</p>';
+        echo '<p>'. $job->obtenerTiempoLaborado($job->meses) . '</p>';
         echo '<strong>Funciones:</strong>';
         echo '<ul>';
         echo '<li>Desarrollar aplicaciones web.</li>';
