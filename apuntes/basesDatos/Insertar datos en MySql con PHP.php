88b51fe20289345aa016a66c423a7ff65feb4771
frodriguez <vargasjuan367@gmail.com>
2019-06-23 22:20:00 -0500

    Conect bd  listar save

---
+++ apuntes/basesDatos/Insertar datos en MySql con PHP.php
@@ -0,0 +1,5 @@
+En esta clase conectaremos una aplicación en PHP con una base de datos MySQL, esto es contenido extra al proyecto del curso. Podrás encontrar el template de esta aplicación en su repositorio de GitHub.
+
+Dentro de nuestro proyecto utilizaremos Eloquent para conectarnos a la base de datos, para ello dentro de la carpeta de nuestro proyecto vamos a correr el comando:
+
+php composer.phar require illuminate/database
