7b7a2b8f9bb2bd03c231b215f4b5b0e795f56096
frodriguez <vargasjuan367@gmail.com>
2019-07-11 22:10:27 -0500

    validaciones

--- app/Controllers/TrabajoController.php
+++ app/Controllers/TrabajoController.php
@@ -1,24 +1,39 @@
 <?php
 namespace App\Controllers;
 use App\Models\experiencia;
+use Respect\Validation\Validator as validacion;
+
 class TrabajoController extends BaseController
 {
-	public function getAddTrabajoAction($request){
+	public function getAddTrabajoAction($request)
+  {
+      $responseMessage = null;
 
+  		if ($request->getMethod() == 'POST')
+  		{
+  			$postData = $request->getParsedBody();
 
-		if ($request->getMethod() == 'POST')
-		{
-			$postData = $request->getParsedBody();
-			$trabajo = new experiencia();
-			$trabajo->titulo = $postData["titulo"];
-			$trabajo->descripcion = $postData["descripcion"];
-			$trabajo->meses = $postData["meses"];
-			$trabajo->save();
+  			$formTrabajoValidator =
+  			validacion::key('titulo', validacion::stringType()->notEmpty())
+                    		->key('descripcion', validacion::stringType()->notEmpty())
+                    		->key('meses', validacion::stringType()->notEmpty());
+        try{
+          $formTrabajoValidator->assert($postData);
+          $trabajo = new experiencia();
+    			$trabajo->titulo = $postData["titulo"];
+    			$trabajo->descripcion = $postData["descripcion"];
+    			$trabajo->meses = $postData["meses"];
+    			$trabajo->save();
+          $responseMessage = 'se guardo la información correctamente';
+        } catch (\Exception $e){
+          $responseMessage = $e->getMessage();
+        }
 
-		}
+  		}
+		    return $this->rendetHTML('agregarTrabajo.twig',[
 
-		return $this->rendetHTML('agregarTrabajo.twig');
-
+            'responseMessage' => $responseMessage
+        ]);
 	}
 
 }
---
+++ apuntes/validaciones/readme.txt
@@ -0,0 +1,5 @@
+Validaciones
+Usaremos esta dependencia: https://packagist.org/packages/respect/validation la cual integraremos a composer.
+
+Haremos validaciones del lado del servidor.
+
